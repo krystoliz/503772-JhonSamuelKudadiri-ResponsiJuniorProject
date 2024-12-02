29bffb297709b6236cf328df775e5189f3aad83c
krystoliz <jihyop67@gmail.com>
2024-12-02 09:26:28 +0700

    setup : CRUD

--- ResponsiJuniorProject/Form1.cs
+++ ResponsiJuniorProject/Form1.cs
@@ -31,26 +31,78 @@ namespace ResponsiJuniorProject
 
         private void btnInsert_Click(object sender, EventArgs e)
         {
+            try
+            {
+                conn.Open();
+                sql = @"select * from st_insert(:_id_karyawan, :_nama)";
+                cmd = new NpgsqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("_id_karyawan", txtDep.Text);
+                cmd.Parameters.AddWithValue("_nama", txtNama.Text);
+                cmd.ExecuteNonQuery();
+                conn.Close();
 
+
+            } catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
 
         private void btnEdit_Click(object sender, EventArgs e)
         {
-            if(r == null)
+            if (r == null)
             {
-                MessageBox.Show("Pilih data yang akan diubah","Good", MessageBoxButtons.OK);
+                MessageBox.Show("Pilih data yang akan diubah", "Good", MessageBoxButtons.OK);
             }
-
+            try
+            {
+                conn.Open();
+                sql = @"select * from st_update(:_nama, :_id_karyawan)";
+                cmd = new NpgsqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("_nama", txtNama.Text);
+                cmd.Parameters.AddWithValue("_id_karyawan", txtDep.Text);
+                cmd.ExecuteNonQuery();
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
 
         private void btnDelete_Click(object sender, EventArgs e)
         {
+            try
+            {
+                conn.Open();
+                sql = @"select * from st_delete(:_id_karyawan)";
+                cmd = new NpgsqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("_id_karyawan", txtDep.Text);
+                cmd.ExecuteNonQuery();
+                conn.Close();
+            }
+            catch
+            {
 
+            }
         }
 
         private void btnLoad_Click(object sender, EventArgs e)
         {
+            try
+            {
+                conn.Open();
+                sql = @"select * from st_select()";
+                cmd = new NpgsqlCommand(sql, conn);
+                dt = new DataTable();
+                dt.Load(cmd.ExecuteReader());
+                dgvData.DataSource = dt;
+                conn.Close();
+            }
+            catch
+            {
 
+            }
         }
     }
 }
